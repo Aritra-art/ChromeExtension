3fa51c4f9c561111e1352e3e940354153ba5774c
aritra <aritrachowdhury65@gmail.com>
2022-10-28 19:45:14 +0530

    changed

--- app.js
+++ app.js
@@ -21,3 +21,26 @@
 // }
 
 // check.addEventListener("click", profileEventHandler);
+const likeNum = document.querySelector("#like");
+const commentNum = document.querySelector("#comment");
+const count = document.querySelector("#count");
+const clear = document.querySelector("#clear");
+
+count.addEventListener("click", (e) => {
+  e.preventDefault();
+  console.log("clicked");
+});
+
+likeNum.addEventListener("input", () => {
+  if (likeNum.value.length > 0) {
+    count.removeAttribute("disabled");
+  } else {
+    count.setAttribute("disabled", "disabled");
+  }
+});
+
+clear.addEventListener("click", (e) => {
+  e.preventDefault();
+  likeNum.value = "";
+  commentNum.value = "";
+});
